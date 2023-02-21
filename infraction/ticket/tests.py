38589dd851a6d5d186bb62024568082f65966bbd
wzunigah <wzunigah@gmail.com>
2023-02-20 19:18:38 -0500

    Adding test fot ticket

--- infraction/ticket/tests.py
+++ infraction/ticket/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.test.client import Client
 
-# Create your tests here.
+class TicketTest(TestCase):
+    def test_get_ticket_by_email(self):
+        response = self.client.get('/generar_informe?email=wzunigah@gmail.com')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_ticket_by_email_no_exist_email(self):
+        response = self.client.get('/generar_informe?email=wzuniga@gmail.com')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('[]' in str(response.content))
